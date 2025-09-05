18889775626de8cb294918760fdbaeab59cd1044
caubinho <caubinho@gmail.com>
2025-09-06 00:38:35 +0200

    Consent II

--- resources/views/layouts/admin/base.blade.php
+++ resources/views/layouts/admin/base.blade.php
@@ -25,12 +25,13 @@
 
         <!-- CSRF Token -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
+
         <!-- Google Tag Manager -->
         <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
                     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
                 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
                 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-PFJFT9RF');</script>
+            })(window,document,'script','dataLayer','GTM-NGW8R7KR');</script>
         <!-- End Google Tag Manager -->
 
 
@@ -50,10 +51,12 @@
     </head>
 
     <body>
+
     <!-- Google Tag Manager (noscript) -->
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PFJFT9RF"
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NGW8R7KR"
                       height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
     <!-- End Google Tag Manager (noscript) -->
+
         @yield('body')
 
     <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/cookieconsent@3/build/cookieconsent.min.css" />
